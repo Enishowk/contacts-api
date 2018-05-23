87db2dc7a9aea624310a09112ff47dfda3eef86d
Enishowk <kenshineuh@gmail.com>
2018-05-23 23:08:12 +0200

    feat: add login route, rework jwt utils

--- routes.js
+++ routes.js
@@ -1,10 +1,18 @@
-const { registerUserAction } = require("./controller/UserController");
+const {
+  registerUserAction,
+  loginUserAction,
+} = require("./controller/UserController");
 
 const AppRoutes = [
   {
     path: "/user/register",
     method: "post",
     action: registerUserAction,
+  },
+  {
+    path: "/user/login",
+    method: "post",
+    action: loginUserAction,
   },
 ];
 
